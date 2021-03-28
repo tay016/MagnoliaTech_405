d56672c8c332d25ac42b2edd664875027401ea7e
Adrian Jenkins <adrian@Adrians-MacBook-Pro.local>
2021-03-28 10:53:25 -0400

    hiearchy creation page

---
+++ src/makeHiearchy.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { API, Storage } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+import { ExportCSV } from './ExportCSV';
