cd1052f548d0cc32a7b9de2f96bfdba178f9db5f
Kelleytina Williams <kelleytina.williams@gmail.com>
2021-04-05 22:19:06 -0400

    minor changes

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,10 +3,10 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:1c1da86c-d1a3-4cf7-9a02-118b25d1319f",
+    "aws_cognito_identity_pool_id": "us-east-1:539caea1-189b-4665-9cca-d2d68c0b9db7",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_NzYo2EUp0",
-    "aws_user_pools_web_client_id": "321f3mq43jqkcl259j09ku910e",
+    "aws_user_pools_id": "us-east-1_pbIFHstai",
+    "aws_user_pools_web_client_id": "3ir46t6uq6cocg2c86fu2ibfjm",
     "oauth": {}
 };
 
