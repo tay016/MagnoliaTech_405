be528930ba2e0139822b0df0c695b18996c7329e
Adrian Jenkins <adrian.jenkins99@gmail.com>
2021-04-08 11:32:23 -0400

    removed express import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from "react-router-dom";
-import express from 'express';
 import './App.css';
 import { API, Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
